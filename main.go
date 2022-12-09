2f1364d172c8f43c6a0dc9b7d507759fd11f0f52
ferares <fermin@ares.uy>
2022-12-09 17:13:41 -0300

    Improve day generator

--- main.go
+++ main.go
@@ -22,18 +22,35 @@ func createDayFromTemplate(day string) {
 	fileName := "./solutions/days/day" + day + ".go"
 	_, err := os.Stat(fileName)
 	if (err == nil) { panic("File '" + fileName + "' already exists!") }
-	in, err := os.Open("./template.go.txt")
+	template, err := os.Open("./template.go.txt")
 	if (err != nil) { panic(err) }
-	defer in.Close()
+	defer template.Close()
 	out, err := os.Create(fileName)
 	if (err != nil) { panic(err) }
 	defer out.Close()
-	content, err := io.ReadAll(in)
+	content, err := io.ReadAll(template)
 	if (err != nil) { panic(err) }
 	newContent := strings.ReplaceAll(string(content), "Day#", "Day" + day)
 	_, err = out.WriteString(newContent)
 	if (err != nil) { panic(err) }
 	err = out.Sync()
+	if (err != nil) { panic(err) }
+	main, err := os.OpenFile("./main.go", os.O_RDWR, os.ModeAppend)
+	if (err != nil) { panic(err) }
+	defer main.Close()
+	content, err = io.ReadAll(main)
+	if (err != nil) { panic(err) }
+	err = main.Truncate(0)
+	if (err != nil) { panic(err) }
+	_, err = main.Seek(0, 0)
+	if (err != nil) { panic(err) }
+	comment := "// case \"#\": daySolution = days.Day#{}"
+	newLine := strings.ReplaceAll(comment, "// ", "")
+	newLine = strings.ReplaceAll(newLine, "#", day) + "\n\t\t" + comment
+	newContent = strings.ReplaceAll(string(content), comment, newLine)
+	_, err = main.WriteString(newContent)
+	if (err != nil) { panic(err) }
+	_, err = os.Create("./input/" + day + ".txt")
 	if (err != nil) { panic(err) }
 }
 
@@ -47,17 +64,22 @@ func runSolution(daySolution solutions.DaySolution, puzzle string, input string)
 	fmt.Println((solution))
 }
 
+func getDaySolution(day string) solutions.DaySolution {
+	var daySolution solutions.DaySolution
+	switch day {
+		case "1": daySolution = days.Day1{}
+		// case "#": daySolution = days.Day#{}
+	}
+	return daySolution
+}
+
 func main() {
 	args := os.Args[1:]
 	if (len(args) == 2) {
 		day := args[0]
 		puzzle := args[1]
 		input := readInput(day)
-		var daySolution solutions.DaySolution
-		switch day {
-		case "1":
-			daySolution = days.Day1{}
-		}
+		daySolution := getDaySolution(day)
 		runSolution(daySolution, puzzle, input)
 	} else if (len(args) == 1) {
 		day := args[0]
